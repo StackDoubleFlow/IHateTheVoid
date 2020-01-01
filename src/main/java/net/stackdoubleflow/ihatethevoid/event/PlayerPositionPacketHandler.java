b58e8394d669adb731300a0e886f86721c423182
Pauli Kauro <3965357+paulikauro@users.noreply.github.com>
2020-01-02 00:54:42 +0200

    Add early returns and extract a few things

--- src/main/java/net/stackdoubleflow/ihatethevoid/event/PlayerPositionPacketHandler.java
+++ src/main/java/net/stackdoubleflow/ihatethevoid/event/PlayerPositionPacketHandler.java
@@ -5,43 +5,49 @@ import com.comphenix.executors.BukkitScheduledExecutorService;
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.events.ListenerPriority;
 import com.comphenix.protocol.events.PacketAdapter;
-import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.reflect.StructureModifier;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PlayerPositionPacketHandler extends PacketAdapter {
 
-    private JavaPlugin plugin;
-    private BukkitScheduledExecutorService syncExecutor;
+    private final BukkitScheduledExecutorService syncExecutor;
 
     public PlayerPositionPacketHandler(JavaPlugin plugin) {
         super(plugin, ListenerPriority.HIGHEST, PacketType.Play.Client.POSITION,
                 PacketType.Play.Client.POSITION_LOOK);
-        this.plugin = plugin;
         this.syncExecutor = BukkitExecutors.newSynchronous(plugin);
     }
 
     @Override
     public void onPacketReceiving(PacketEvent event) {
-        if (event.getPacketType() == PacketType.Play.Client.POSITION_LOOK ||
-                event.getPacketType() == PacketType.Play.Client.POSITION) {
-            PacketContainer packet = event.getPacket();
-            double x = packet.getDoubles().read(0);
-            double y = packet.getDoubles().read(1);
-            double z = packet.getDoubles().read(2);
-            if(!Double.isFinite(x) || !Double.isFinite(y) || !Double.isFinite(z) ||
-                    Math.abs(x) >= 3.0E7 || Math.abs(z) >= 3.0E7 || Math.abs(y) >= 3.0E7) {
-                event.setCancelled(true);
-                syncExecutor.execute(new Runnable() {
-                    @Override
-                    public void run() {
-                        event.getPlayer().teleport(event.getPlayer().getWorld().getSpawnLocation());
-                        event.getPlayer().sendMessage(ChatColor.GOLD.toString() + ChatColor.BOLD.toString() +
-                                "You were sent back to spawn due to an invalid location.");
-                    }
-                });
-            }
+        if (event.getPacketType() != PacketType.Play.Client.POSITION_LOOK &&
+                event.getPacketType() != PacketType.Play.Client.POSITION) {
+            return;
         }
+        StructureModifier<Double> doubles = event.getPacket().getDoubles();
+        double x = doubles.read(0);
+        double y = doubles.read(1);
+        double z = doubles.read(2);
+        if (isValid(x) && isValid(y) && isValid(z)) {
+            return;
+        }
+        event.setCancelled(true);
+        syncExecutor.execute(() -> {
+            fixPlayer(event.getPlayer());
+        });
+    }
+
+    static boolean isValid(double d) {
+        return Double.isFinite(d) && Math.abs(d) < 3.0E7;
+    }
+
+    public static void fixPlayer(Player player) {
+        player.teleport(player.getWorld().getSpawnLocation());
+        player.sendMessage(
+                ChatColor.GOLD.toString() + ChatColor.BOLD.toString() +
+                        "You were sent back to spawn due to an invalid location.");
     }
 }
